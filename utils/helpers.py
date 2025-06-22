f80365a0b3a1dff71b39d909f398c814c7774444
Luca Butera <113059340+lbutera04@users.noreply.github.com>
2025-06-22 18:43:05 -0400

    update utils

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,11 +3,32 @@ import osmnx as ox
 import networkx as nx
 import geopandas as gpd
 
-def generate_graph(region):
-    geom = ox.geocode_to_gdf(region)
+def fill_holes_and_dissolve(geom):
+    """
+    Remove interior rings and dissolve overlaps of a polygon
+    Args:
+        geom (shapely.Polygon, shapely.MultiPolygon): The polygon to be filled in
+    Returns:
+        cleaned.buffer[0] (shapely.Polygon, shapely.MultiPolygon): The cleaned polygon
+    """
     if geom.geom_type == "Polygon":
         cleaned = Polygon(geom.exterior)
     elif geom.geom_type == "MultiPolygon":
         cleaned = MultiPolygon([Polygon(p.exterior) for p in geom.geoms])
-    graph = ox.graph_from_polygon(cleaned.buffer(0), network_type='drive_service')
+    else:          
+        return geom
+    return cleaned.buffer(0) 
+
+def generate_graph(region):
+    """
+    Generate a graph from a region
+    Parameters:
+        region : string
+            The name of the region, formatted for ox.geocode_to_gdf()
+    Outputs:
+        graph : nx.MultiDiGraph
+            Graph from the region's outermost polygon
+    """
+    geom = ox.geocode_to_gdf(region)
+    graph = geom.apply(fill_holes_and_dissolve)
     return graph
